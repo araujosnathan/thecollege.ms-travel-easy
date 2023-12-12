2be0a5f9ebdb15e793f583c47314c0be9813094c
nathanaelaraujo <nathanael.araujos@gmail.com>
2023-12-12 21:31:13 +0000

    Remoção de arquivo newt.txt e adição de nova rota para uma controller de Bookings

---
+++ ms-travel-easy/src/Controllers/V1/BookingsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ms_travel_easy.src.Models;
+using ms_travel_easy.src.Services;
+
+namespace ms_travel_easy.src.Controllers
+{
+    [ApiController]
+    [Route("api/v1/Customer/[controller]")]
+    public class BookingsController : ControllerBase
+    {
+
+        [HttpGet()]
+        public async Task<ActionResult> Get()
+        {
+            return Ok();
+        }
+
+
+    }
+}
+
